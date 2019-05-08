98970aa4f93649eede0ac49a0e9129b1365aacc0
Pavel Ivanov <ivanovpavelalex45@gmail.com>
2019-05-08 16:22:00 +0500

    LibevLoop: implemented

--- loop/libev_loop.py
+++ loop/libev_loop.py
@@ -1,16 +1,122 @@
-from mood.event import EvLoop
+import mood.event as libev
 
 from loop.tick import FutureTickQueue
 from loop.signal import Signals
+from loop.timer import Timer
 
 
 class LibevLoop:
     def __init__(self):
-        self.loop = EvLoop()
+        self.loop = libev.Loop()
         self.future_tick_queue = FutureTickQueue()
-        self.timers = None # SplObjectStorage
-        self.read_streams = None # EvIo[]
-        self.write_streams = None # EvIo[]
+        self.timers = {}
+        self.read_streams = {}
+        self.write_streams = {}
         self.running = False
         self.signals = Signals()
-        self.signal_events = None # EvSignal[]
+        self.signal_events = {}
+
+    def add_read_stream(self, stream, listener):
+        key = hash(stream)
+        if key not in self.read_streams:
+            ev_io = self.loop.io(
+                stream,
+                libev.EV_READ,
+                lambda *args: listener(stream)
+            )
+            ev_io.start()
+            self.read_streams[key] = ev_io
+
+    def add_write_stream(self, stream, listener):
+        key = hash(stream)
+        if key not in self.write_streams:
+            ev_io = self.loop.io(
+                stream,
+                libev.EV_WRITE,
+                lambda *args: listener(stream)
+            )
+            ev_io.start()
+            self.write_streams[key] = ev_io
+
+    def remove_read_stream(self, stream):
+        key = hash(stream)
+        if key in self.read_streams:
+            self.read_streams[key].stop()
+            del self.read_streams[key]
+
+    def remove_write_stream(self, stream):
+        key = hash(stream)
+        if key in self.write_streams:
+            self.write_streams[key].stop()
+            del self.write_streams[key]
+
+    def add_timer(self, interval, callback):
+        timer = Timer(interval, callback)
+        key = hash(timer)
+
+        def action(*args):
+            nonlocal timer
+            timer.callback()
+            self.cancel_timer(timer)
+        ev_timer = self.loop.timer(timer.interval, 0.0, action)
+        ev_timer.start()
+        self.timers[key] = ev_timer
+        return timer
+
+    def add_periodic_timer(self, interval, callback):
+        timer = Timer(interval, callback)
+        key = hash(timer)
+        ev_timer = self.loop.timer(
+            timer.interval,
+            timer.interval,
+            timer.callback
+        )
+        ev_timer.start()
+        self.timers[key] = ev_timer
+        return timer
+
+    def cancel_timer(self, timer):
+        key = hash(timer)
+        if key in self.timers:
+            self.timers[key].stop()
+            del self.timers[key]
+
+    def future_tick(self, listener):
+        self.future_tick_queue.add(listener)
+
+    def stop(self):
+        self.running = False
+
+    def add_signal(self, signum, listener):
+        self.signals.add(signum, listener)
+        if signum not in self.signal_events:
+            ev_signal = self.loop.signal(
+                signum,
+                lambda *args: self.signals.call(signum)
+            )
+            ev_signal.start()
+            self.signal_events[signum] = ev_signal
+
+    def remove_signal(self, signum, listener):
+        self.signals.remove(signum, listener)
+        if not self.signals.count(signum):
+            self.signal_events[signum].stop()
+            del self.signal_events[signum]
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.future_tick_queue.tick()
+            has_pending_callbacks = not self.future_tick_queue.empty()
+            was_just_stopped = not self.running
+            nothing_left_to_do = (not self.read_streams and
+                                  not self.write_streams and
+                                  not self.timers and
+                                  self.signals.empty())
+
+            if was_just_stopped or has_pending_callbacks:
+                self.loop.start(libev.EVRUN_NOWAIT)
+            elif nothing_left_to_do:
+                break
+            else:
+                self.loop.start(libev.EVRUN_ONCE)
--- tests/test_select_loop.py
+++ tests/test_select_loop.py
@@ -9,8 +9,12 @@ from loop.select_loop import SelectLoop
 
 
 class TestSelectLoop(unittest.TestCase):
+    @staticmethod
+    def create_loop():
+        return SelectLoop()
+
     def setUp(self):
-        self.loop = SelectLoop()
+        self.loop = self.create_loop()
         self.mock = unittest.mock.Mock()
         (self.rstream, self.wstream) = self.create_socket_pair()
 
@@ -18,24 +22,22 @@ class TestSelectLoop(unittest.TestCase):
         self.rstream.close()
         self.wstream.close()
 
+    def test_loop_without_resources(self):
+        self.assert_run_faster_than(0.02)
+
     def test_future_tick_handler_can_cancel_registered_stream(self):
         self.loop.add_write_stream(self.wstream, self.mock)
         self.loop.future_tick(
             lambda: self.loop.remove_write_stream(self.wstream)
         )
         self.loop.run()
-
         self.mock.assert_not_called()
-
-    def test_loop_without_resources(self):
-        self.assert_run_faster_than(0.02)
 
     def test_add_write_stream_ignore_second_callable(self):
         self.loop.add_write_stream(self.wstream, lambda stream: self.mock(1))
-        self.loop.add_write_stream(self.wstream, lambda stream: self.mock("ignore"))
+        self.loop.add_write_stream(self.wstream, lambda stream: self.mock(2))
         self.next_tick()
         self.next_tick()
-
         self.assertEqual(
             [unittest.mock.call(1), unittest.mock.call(1)],
             self.mock.call_args_list
@@ -47,13 +49,10 @@ class TestSelectLoop(unittest.TestCase):
             lambda stream: self.mock(stream.recv(1024))
         )
         self.loop.add_read_stream(self.rstream, lambda stream: self.mock("ignore"))
-
         self.wstream.send(b"hello")
         self.next_tick()
-
         self.wstream.send(b"world")
         self.next_tick()
-
         self.assertEqual(
             [unittest.mock.call(b"hello"), unittest.mock.call(b"world")],
             self.mock.call_args_list
@@ -63,7 +62,6 @@ class TestSelectLoop(unittest.TestCase):
         self.loop.add_write_stream(self.wstream, self.mock)
         self.next_tick()
         self.next_tick()
-
         self.assertEqual(2, self.mock.call_count)
 
     def test_add_read_stream(self):
@@ -72,7 +70,6 @@ class TestSelectLoop(unittest.TestCase):
         self.next_tick()
         self.wstream.send(b"world")
         self.next_tick()
-
         self.assertEqual(2, self.mock.call_count)
 
     def test_select_loop_timeout_emulation(self):
@@ -81,7 +78,6 @@ class TestSelectLoop(unittest.TestCase):
         self.loop.run()
         end = time.time()
         interval = end - start
-
         self.mock.assert_called_once()
         self.assertLessEqual(0.04, interval)
 
@@ -89,7 +85,6 @@ class TestSelectLoop(unittest.TestCase):
         timer = self.loop.add_periodic_timer(0.05, self.mock)
         self.loop.add_timer(0.12, lambda: self.loop.cancel_timer(timer))
         self.loop.run()
-
         self.assertEqual(2, self.mock.call_count)
 
     def test_remove_read_stream_instantly(self):
@@ -97,7 +92,6 @@ class TestSelectLoop(unittest.TestCase):
         self.loop.remove_read_stream(self.rstream)
         self.wstream.send(b"bar")
         self.next_tick()
-
         self.mock.assert_not_called()
 
     def test_remove_read_stream_after_reading(self):
@@ -107,14 +101,12 @@ class TestSelectLoop(unittest.TestCase):
         self.loop.remove_read_stream(self.rstream)
         self.wstream.send(b"bar")
         self.next_tick()
-
         self.mock.assert_called_once()
 
     def test_remove_write_stream_instantly(self):
         self.loop.add_write_stream(self.wstream, self.mock)
         self.loop.remove_write_stream(self.wstream)
         self.next_tick()
-
         self.mock.assert_not_called()
 
     def test_remove_write_stream_after_writing(self):
@@ -122,7 +114,6 @@ class TestSelectLoop(unittest.TestCase):
         self.next_tick()
         self.loop.remove_write_stream(self.wstream)
         self.next_tick()
-
         self.mock.assert_called_once()
 
     def test_read_stream_removes_itself(self):
@@ -134,7 +125,6 @@ class TestSelectLoop(unittest.TestCase):
         self.loop.add_read_stream(self.rstream, remove_itself)
         self.wstream.send(b"hello")
         self.loop.run()
-
         self.assertEqual(
             [unittest.mock.call(1), unittest.mock.call(0)],
             self.mock.call_args_list
@@ -148,20 +138,18 @@ class TestSelectLoop(unittest.TestCase):
 
         self.loop.add_write_stream(self.wstream, remove_itself)
         self.loop.run()
-
         self.assertEqual(
             [unittest.mock.call(1), unittest.mock.call(0)],
             self.mock.call_args_list
         )
 
-    def test_without_called(self):
+    def test_cleanup_before_mock_call(self):
         def cleanup(stream):
             self.loop.remove_read_stream(stream)
             self.loop.remove_write_stream(stream)
 
         self.loop.add_read_stream(self.rstream, self.mock)
         self.loop.add_write_stream(self.rstream, cleanup)
-
         self.assert_run_faster_than(0.02)
         self.mock.assert_not_called()
 
@@ -169,7 +157,6 @@ class TestSelectLoop(unittest.TestCase):
         self.loop.add_read_stream(self.rstream, self.mock)
         self.wstream.close()
         self.next_tick()
-
         self.mock.assert_called_once()
 
     def test_read_streams_are_handled_earlier_than_write_streams(self):
@@ -184,7 +171,6 @@ class TestSelectLoop(unittest.TestCase):
         self.loop.add_write_stream(stream, cleanup)
         another.close()
         self.loop.run()
-
         self.assertEqual(
             [unittest.mock.call("read"), unittest.mock.call("write")],
             self.mock.call_args_list
@@ -207,7 +193,6 @@ class TestSelectLoop(unittest.TestCase):
         self.loop.future_tick(lambda: self.mock(1))
         self.loop.future_tick(lambda: self.mock(2))
         self.loop.run()
-
         self.assertEqual(
             [unittest.mock.call(1), unittest.mock.call(2)],
             self.mock.call_args_list
@@ -230,7 +215,6 @@ class TestSelectLoop(unittest.TestCase):
         self.loop.future_tick(lambda: self.mock("tick 1"))
         self.loop.future_tick(lambda: self.mock("tick 2"))
         self.loop.run()
-
         self.assertEqual(
             [unittest.mock.call("tick 1"),
              unittest.mock.call("tick 2"),
@@ -241,7 +225,6 @@ class TestSelectLoop(unittest.TestCase):
         )
 
     def test_signal(self):
-
         def cleanup():
             nonlocal timer
             self.loop.remove_signal(signal.SIGUSR1, cleanup)
@@ -278,11 +261,11 @@ class TestSelectLoop(unittest.TestCase):
         self.mock.assert_not_called()
 
     def test_only_signals(self):
-
         def cleanup():
             self.mock(2)
             self.loop.remove_signal(signal.SIGUSR1, listener)
             self.loop.remove_signal(signal.SIGUSR2, cleanup)
+
         listener = lambda: self.mock(1)
         self.loop.add_signal(signal.SIGUSR1, listener)
         self.loop.add_signal(signal.SIGUSR2, cleanup)
@@ -308,7 +291,6 @@ class TestSelectLoop(unittest.TestCase):
     def test_timer_inteval_can_be_far_in_future(self):
         timer = self.loop.add_timer(10 ** 6, self.mock)
         self.loop.future_tick(lambda: self.loop.cancel_timer(timer))
-
         self.assert_run_faster_than(0.02)
 
     def create_socket_pair(self):
@@ -321,6 +303,8 @@ class TestSelectLoop(unittest.TestCase):
         wstream.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 0)
         rstream.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 0)
         wstream.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 0)
+        rstream.setblocking(False)
+        wstream.setblocking(False)
         return (rstream, wstream)
 
     def next_tick(self):
